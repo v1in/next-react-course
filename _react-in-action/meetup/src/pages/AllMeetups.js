44a672cdb2095044336dfdfac10dea376eb47e6d
VA <inpersona.v@gmail.com>
2021-03-21 00:35:04 +0100

    Added GET Http Request, fetching data from firebase

--- _react-in-action/meetup/src/pages/AllMeetups.js
+++ _react-in-action/meetup/src/pages/AllMeetups.js
@@ -1,31 +1,48 @@
+import { useState, useEffect } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image: "https://picsum.photos/id/237/400/200.webp",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image: "https://picsum.photos/id/238/400/200.webp",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+export default function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-const AllMeetupsPage = () => {
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`${process.env.REACT_APP_MEETUPS_API_URL}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
       <ul>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={loadedMeetups} />
       </ul>
     </section>
   );
-};
-
-export default AllMeetupsPage;
+}
