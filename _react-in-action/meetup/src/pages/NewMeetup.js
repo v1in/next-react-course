fbcc74d1c6795bc490f35ed5bee2ca7f2254b19a
VA <inpersona.v@gmail.com>
2021-03-20 23:40:15 +0100

    Added POST Http Request

--- _react-in-action/meetup/src/pages/NewMeetup.js
+++ _react-in-action/meetup/src/pages/NewMeetup.js
@@ -1,12 +1,27 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-    return (
-        <section>
-            <h1>Add New Meetup</h1>
-            <NewMeetupForm />
-        </section>
-    )
-}
+  const history = useHistory();
+  const addMeetupHandler = (meetupData) => {
+    fetch(`${process.env.REACT_APP_MEETUPS_API_URL}`, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
+    });
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
 
 export default NewMeetupPage;
