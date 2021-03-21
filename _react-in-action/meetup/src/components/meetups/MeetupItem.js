a23bc9443e002295226e00364cdc426818768085
VA <inpersona.v@gmail.com>
2021-03-21 13:59:53 +0100

    [react-in-action/meetup] added React Context w/ Favorites, check previous state

--- _react-in-action/meetup/src/components/meetups/MeetupItem.js
+++ _react-in-action/meetup/src/components/meetups/MeetupItem.js
@@ -1,7 +1,27 @@
+import { useContext } from "react";
+
 import Card from "../ui/Card";
 import classes from "./styles/MeetupItem.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 export default function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toogleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -15,7 +35,9 @@ export default function MeetupItem(props) {
             <p>{props.description}</p>
           </div>
           <div className={classes.actions}>
-            <button>To Favorites</button>
+            <button onClick={toogleFavoriteStatusHandler}>
+              {itemIsFavorite ? "Remove form Favorites" : "To Favorites"}
+            </button>
           </div>
         </div>
       </Card>
