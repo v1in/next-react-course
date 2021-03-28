1f6b94cf87cd2fbda4fa10410e54d98a073b9074
VA <inpersona.v@gmail.com>
2021-03-28 18:21:02 +0200

    [routing] add navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,10 +5,19 @@ export default function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // load data ...
+    router.push({
+      pathname: "/clients/[id]/[clientprojectId]",
+      query: { id: "client1", clientprojectId: "project1" },
+    });
+  }
+
   return (
     <div>
       <Logo />
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project</button>
     </div>
   );
 }
