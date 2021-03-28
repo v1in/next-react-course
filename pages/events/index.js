a0d76a2ad80e30ac0ce8d60f2893fdd7d5662ccb
VA <inpersona.v@gmail.com>
2021-03-28 19:11:16 +0200

    [events] add main page for Events project

---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+import styles from "../../styles/events.module.css";
+
+export default function AllEventsPage() {
+  return (
+    <div className={styles.container}>
+      <h1>All Events</h1>
+    </div>
+  );
+}
