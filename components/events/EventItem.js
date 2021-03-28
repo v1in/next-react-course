fc632fac47834a60641bc0fc2ce9730c1ccf6420
VA <inpersona.v@gmail.com>
2021-03-28 23:10:21 +0200

    [events] upd styles, add icons and button

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import DateIcon from "../icons/DateIcon";
+import AddressIcon from "../icons/AddressIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 
 import classes from "./EventItem.module.css";
 
@@ -21,14 +24,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
