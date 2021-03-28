bf5b351178ff3c196f30232438d018b7910d0313
VA <inpersona.v@gmail.com>
2021-03-28 23:12:47 +0200

    [events] fix url for Button

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,4 @@
-import Button from "../ui/button";
+import Button from "../ui/Button";
 import DateIcon from "../icons/DateIcon";
 import AddressIcon from "../icons/AddressIcon";
 import ArrowRightIcon from "../icons/ArrowRightIcon";
