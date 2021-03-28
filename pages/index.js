c11f8a930769b66a408a45f763d4ff4519a15cef
VA <inpersona.v@gmail.com>
2021-03-28 16:38:56 +0200

    [routing] upd Links

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,16 @@ export default function HomePage() {
       <h1>The Home Page</h1>
       <ul>
         <li>
-          <Link href="/projects">
-            <a>Projects</a>
-          </Link>
+          <Link href="/projects">Projects</Link>
         </li>
         <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
+          <Link href="/about">About</Link>
         </li>
         <li>
-          <Link href="/clients">
-            <a>Clients</a>
-          </Link>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="/blog/some-blog-post">Blog</Link>
         </li>
       </ul>
     </div>
