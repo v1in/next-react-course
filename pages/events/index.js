c074398b342c19a36a35db548ead5266c121fab9
VA <inpersona.v@gmail.com>
2021-04-10 17:04:16 +0200

    [events] update All Events list

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,14 @@
-import { getFeaturedEvents } from "../../dummy-data";
+import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import Layout from "../../components/layout/layout"; // For example, added for all events pages
 
 export default function AllEventsPage() {
-  const featuredEvents = getFeaturedEvents();
+  const events = getAllEvents();
 
   return (
     <Layout>
       <h1>All Events</h1>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </Layout>
   );
 }
