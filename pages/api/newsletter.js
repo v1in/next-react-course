6e6657be1d7d70f6aa7f9aa804332d1877f820de
Vladimir V <inpersona.v@gmail.com>
2021-12-05 15:23:55 +0100

    [events][api-route] connect comments into mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -10,12 +10,12 @@ async function handler(req, res) {
     }
 
     const client = await MongoClient.connect(
-      'mongodb+srv://dbAdmin:ljdwOtNWz9oIxpbP@vldmrsandbox.kcf79.mongodb.net/newsletter?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vldmrsandbox.kcf79.mongodb.net/events?retryWrites=true&w=majority`,
     );
 
     const db = client.db();
 
-    await db.collection('emails').insertOne({email: userEmail});
+    await db.collection('newsletter').insertOne({email: userEmail});
 
     client.close();
 
