df08cb7b07ac19719679cccbf36abadd87d261be
Vladimir V <inpersona.v@gmail.com>
2021-12-12 01:23:06 +0100

    [events][context] add notification store, update codebase

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -1,10 +1,24 @@
+import {useContext} from 'react';
+import NotificationContext from '../../store/notification-store';
+import Notification from '../ui/Notification';
 import MainHeader from './MainHeader';
 
 export default function MainLayout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main style={{paddingBottom: '5rem'}}>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
